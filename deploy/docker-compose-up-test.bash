baad11cb781168f7bc80d491e29c75eb0dcafe02
Roy Jung <phi4grv@gmail.com>
2016-04-22 02:15:13 +1000

    Update docker-compose scripts

--- deploy/docker-compose-up-test.bash
+++ deploy/docker-compose-up-test.bash
@@ -18,7 +18,7 @@ ROOT_DIR=$( dirname $SCRIPT_DIR )
 DOCKER_COMPOSE_FILE=${SCRIPT_DIR}/docker-compose-test.yml
 
 docker-compose -f ${DOCKER_COMPOSE_FILE} up -d db
-sleep 2
+docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm auth-api dockerize -wait tcp://auth-db:5432
 docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm auth-api bin/rails db:setup
 docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm users-api bin/rails db:setup
 docker-compose -f ${DOCKER_COMPOSE_FILE} up -d auth-api users-api users-ui
